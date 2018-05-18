02435703b189c08b83fa27f935ac4fac5d98b582
noaione <34302902+aimanmh@users.noreply.github.com>
2018-05-19 05:51:45 +0700

    Version 1.4
    Reworked hdr2sdr, now using hable tonemap algorithm that stolen from age@Doom9 with some additional command. Added some aliases

--- README.md
+++ README.md
@@ -5,26 +5,27 @@ How to install:
 1. Download/Clone this repo
 2. Extract the files
 3. Copy n4ofunc.py to \Python36\Lib\site-packages
-4. Load it at vapoursynth, ex: import n4ofunc as nao
+4. Load it, ex: import n4ofunc as nao
 ```
 
 (stolen) Feature:
 - Deband (mSdeband, Sdeband, Mdeband, Hdeband)
-- KNLM (Mknlm, Hknlm) #Not Working Properly
+- KNLM (Mknlm, Hknlm) # Not Working Properly
 - BM3D (Nbm3d, Hbm3d)
 - SMDegrain (Nsmd, Hsmd)
 - AA, using vsTAAmbk (Nnedi3taa, eedi3taa, Ntaa, Htaa)
 - deblock
 - dehardsub
-- IVTC, Decimate, Resize to 720p (animu)
-- hdr2sdr (experimental)
-- benchmark (random shit)
+- IVTC, Decimate, Resize to 720p (animu) [aliases: animu, ivtc]
+- hdr2sdr (experimental) [Hable Tonemapping Algorithm, should now provide better result] [Aliases: tonemap, hdr2sdr]
+- benchmark (random things, just put n4ofunc.benchmark(src) and you're done) [Aliases: autistCode, benchmark]
 - i444 meme (i444)
 - descale_rescale
 - Hybrid denoise (S_hybriddenoise, M_hybriddenoise, H_hybriddenoise)
 
 Problem:
 - Everything is a problem
+- KNLM still broken
 - Brokes after 1 preview (Kinda fixed)
 
 Version rev:
@@ -33,3 +34,4 @@ Version rev:
 - Version 1.2: Some changes for High Deband, cleaned up script. Changed argument for i444
 - Version 1.3: Added descale_rescale, reworked autistCode and renamed it to benchmark
 - Version 1.3.1: Fixed KNLMeansCL, some tweak and clean up
+- Version 1.4: Reworked hdr2sdr, now using hable tonemap algorithm that stolen from age@Doom9 with some additional command. Added some aliases
--- n4ofunc.py
+++ n4ofunc.py
@@ -19,25 +19,25 @@ def to_plane_array(clip):
 	return [core.std.ShufflePlanes(clip, x, colorfamily=vs.GRAY) for x in range(clip.format.num_planes)]
 
 #deband
-def mSdeband(optIN : vs.VideoNode):
+def mSdeband(optIN):
 	"""
 	optIN: Input video
 	Mini-mini version of Deband
 	"""
 	return f3kdb.Fag3kdb(optIN)
-def Sdeband(optIN : vs.VideoNode):
+def Sdeband(optIN):
 	"""
 	optIN: Input video
 	Mini version of Deband
 	"""
 	return f3kdb.Fag3kdb(optIN, radiusy=12, radiusc=8, thry=60, thrc=40, grainy=15, grainc=0)
-def Mdeband(optIN : vs.VideoNode):
+def Mdeband(optIN):
 	"""
 	optIN: Input video
 	Normal version of Deband
 	"""
 	return f3kdb.Fag3kdb(optIN, radiusy=18, radiusc=12, thry=60, thrc=50, grainy=15, grainc=0)
-def Hdeband(optIN : vs.VideoNode):
+def Hdeband(optIN):
 	"""
 	optIN: Input video
 	High version of Deband
@@ -50,35 +50,23 @@ def Hdeband(optIN : vs.VideoNode):
 	return out
 
 #denoise
-def Mknlm(optIN : vs.VideoNode, gpu=0):
+def Mknlm(optIN, gpu=0):
 	"""
 	optIN: Input video
-	gpu = 0 for disable using gpu, 1 for enable using gpu
+	Uses GPU for encoding
 	Normal/Medium version of KNLM
 	Better than SMDegrain, worse than BM3D
 	"""
-	if gpu == 1:
-		denoise = [core.knlm.KNLMeansCL(optIN, a=2, h=0.25, d=3, channels="YUV", device_type='gpu', device_id=0)]
-	elif gpu == 0:
-		denoise = [core.knlm.KNLMeansCL(optIN, a=2, h=0.25, d=3, channels="YUV")]
-	else:
-		raise ValueError("Number defined is out of range")
-	return denoise
-def Hknlm(optIN : vs.VideoNode, gpu=0):
+	return core.knlm.KNLMeansCL(optIN, a=2, h=0.25, d=3, channels="YUV", device_type='gpu', device_id=0)
+def Hknlm(optIN):
 	"""
 	optIN: Input video
-	gpu = 0 for disable using gpu, 1 for enable using gpu
+	Uses GPU for Encoding
 	High version of KNLM
 	Better than SMDegrain, worse than BM3D
 	"""
-	if gpu == 1:
-		denoise = [core.knlm.KNLMeansCL(optIN, a=2, h=0.4, d=3, s=8, channels="YUV", device_type='gpu', device_id=0)]
-	elif gpu == 0:
-		denoise = [core.knlm.KNLMeansCL(optIN, a=2, h=0.4, d=3, s=8, channels="YUV")]
-	else:
-		raise ValueError("Number defined is out of range")
-	return denoise
-def Nbm3d(optIN : vs.VideoNode):
+	return core.knlm.KNLMeansCL(optIN, a=2, h=0.4, d=3, s=8, channels="YUV", device_type='gpu', device_id=0)
+def Nbm3d(optIN):
 	"""
 	optIN: Input video
 	Normal/Medium version of BM3D
@@ -86,7 +74,7 @@ def Nbm3d(optIN : vs.VideoNode):
 	REALLY REALLY SLOW, BUT SO FUCKING GOOD
 	""" 
 	return core.bm3d.Basic(optIN)
-def Hbm3d(optIN : vs.VideoNode):
+def Hbm3d(optIN):
 	"""
 	optIN: Input video
 	High version of BM3D
@@ -94,13 +82,13 @@ def Hbm3d(optIN : vs.VideoNode):
 	REALLY REALLY SLOW, BUT SO FUCKING GOOD
 	"""
 	return mvf.BM3D(optIN, sigma=[4,0], radius1=2, matrix=6)
-def Nsmd(optIN : vs.VideoNode):
+def Nsmd(optIN):
 	"""
 	optIN: Input video
 	Normal/Medium version of SMDegrain
 	"""
 	return hvs.SMDegrain(optIN, tr=2,thSAD=50,thSADC=75)
-def Hsmd(optIN : vs.VideoNode):
+def Hsmd(optIN):
 	"""
 	optIN: Input video
 	High version of SMDegrain
@@ -146,27 +134,27 @@ def H_hybriddenoise(src, knl=0.4, sigma=2, radius1=1):
 	return core.std.ShufflePlanes(clips=planes, planes=[0, 0, 0], colorfamily=vs.YUV)
 
 #anti-aliasing
-def Nnedi3taa(optIN : vs.VideoNode, cycle=0):
+def Nnedi3taa(optIN, cycle=0):
 	"""
 	optIN: Input video
 	cycle = (0-1) - 1 is nightmare
 	Fast, and good enough
 	"""
 	return taa.TAAmbk(optIN,aatype='Nnedi3',cycle=cycle)
-def eedi3taa(optIN : vs.VideoNode, cycle=0):
+def eedi3taa(optIN, cycle=0):
 	"""
 	optIN: Input video
 	cycle = (0-1) - 1 is nightmare
 	Slow, but really good, cycle is disabled default
 	"""
 	return taa.TAAmbk(optIN,aatype='Eedi3',cycle=cycle)
-def Ntaa(optIN : vs.VideoNode):
+def Ntaa(optIN):
 	"""
 	optIN: Input video
 	Normal kinda one, tho it may broke on some pc
 	"""
 	return taa.TAAmbk(optIN,aatype=1,mtype=1,strength=0.2,postaa=True,cycle=0)
-def Htaa(optIN : vs.VideoNode): #caution, autist level bypassing through the fucking roof
+def Htaa(optIN): #pre-caution, lag.
 	"""
 	optIN: Input video
 	!!!WARNING!!!
@@ -175,7 +163,7 @@ def Htaa(optIN : vs.VideoNode): #caution, autist level bypassing through the fuc
 	return taa.TAAmbk(optIN,aatype=1,mtype=1,strength=0.3,postaa=True,cycle=1)
 
 #other
-def deblock(optIN : vs.VideoNode):
+def deblock(optIN):
 	"""
 	optIN: Input video
 	Deblocking stuff for your video
@@ -183,7 +171,7 @@ def deblock(optIN : vs.VideoNode):
 	return fvf.AutoDeblock(optIN)
 
 #resize
-def i444(optIN : vs.VideoNode, w=1280, h=720):
+def i444(optIN, w=1280, h=720):
 	"""
 	optIN: Input Video
 	w: Width Resolution
@@ -191,7 +179,7 @@ def i444(optIN : vs.VideoNode, w=1280, h=720):
 	Hi444PP meme resize filter, using spline36 as kernely and blackmanminlobe for kerneluv
 	"""
 	return fvf.Downscale444(optIN, w, h, kernely='blackmanminlobe', kerneluv='blackmanminlobe')
-def descale_rescale(src : vs.VideoNode, w: int, h: int, yuv444=False):
+def descale_rescale(src, w: int, h: int, yuv444=False):
 	"""
 	optIN: Input video
 	w: Width
@@ -199,16 +187,20 @@ def descale_rescale(src : vs.VideoNode, w: int, h: int, yuv444=False):
 	Some descale -> upscale -> descale filter.
 	Set w & h to native resolution, using debilinear descale for this, enable i444 if you want i444
 	"""
-	descale1 = fvf.DebicubicM(src, w, h, b=0, c=1)
-	rpow = edi.nnedi3_rpow2(descale1,rfactor=2) #why not?
-	if yuv444:
-		rescale = i444(rpow,w,h)
-	else:
-		rescale = fvf.Despline36M(rpow, w, h)
+	if w is not None and h is not None:
+		w = src.ViedoNode.width if w is None else w
+		h = src.VideoNode.height if h is None else h
+
+		descale1 = fvf.DebicubicM(src, w, h, b=0, c=1)
+		rpow = edi.nnedi3_rpow2(descale1,rfactor=2) #why not?
+		if yuv444:
+			rescale = i444(rpow,w,h)
+		else:
+			rescale = fvf.Despline36M(rpow, w, h)
 	return rescale
 
 #etc
-def dehardsub(softVideo : vs.VideoNode, hardVideo : vs.VideoNode):
+def dehardsub(softVideo, hardVideo):
 	"""
 	softVideo: Soft-subbed video
 	hardVideo: Hard-subbed video
@@ -218,7 +210,7 @@ def dehardsub(softVideo : vs.VideoNode, hardVideo : vs.VideoNode):
 	mask = fvf.Depth(mask, 16)
 	return core.std.MaskedMerge(hardVideo,softVideo,mask)
 
-def animu(optIN: vs.VideoNode, field=1, order=1, mode=3):
+def animu(optIN, field=1, order=1, mode=3):
 	"""
 	Anime IVTC filterino
 	optIN: Input Video
@@ -231,25 +223,124 @@ def animu(optIN: vs.VideoNode, field=1, order=1, mode=3):
 	animu = [core.vivtc.VDecimate(dein)]
 	return core.resize.Spline36(animu, 1280, 720, format=vs.YUV420P10)
 
-def hdr2sdr(optIN: vs.VideoNode):
+def hdr2sdr(clip,peak=1000,desat=50,brightness=40,cont=1.05,lin=True,show_satmask=False,show_clipped=False):
+	import adjust
 	"""
-	HDR2SDR Conversion
-	optIN: Video input
-	!!!WARNING!!!
-	VERY EXPERIMENTAL THINGY RIGHT HERE
+	HDR102SDR conversion, using Hable tonemap as it base (Stolen from age@Doom9)
+	clip: Video Source
+	peak: Video Peak
+	desat: Desaturation (I recommend leaving it at 50)
+	brightness: Brightness for adjustment (set it to 0 for disable)
+	cont: idk what is this, it makes it better tho
+	lin: Linearize the tonemap
+	satmask: Show Saturation mask (useless)
+	clipped: idk (useless)
 	"""
-	dither32 = fvf.Depth(optIN, 32) #dither to 32bit because tonemap only supported this bitdepth
-	mobius = [core.tonemap.Mobius(dither32, exposure=1.35, transition=10, peak=25)]
-	return core.tonemap.Reinhard(mobius, exposure=2.0, contrast=0.45, peak=1.7)
+	core = vs.get_core()
+	c=clip
+	c=core.resize.Bicubic(clip=c, format=vs.RGBS,filter_param_a=0.0,filter_param_b=1.0, range_in_s="limited", matrix_in_s="2020ncl", primaries_in_s="2020", primaries_s="2020", transfer_in_s="st2084", transfer_s="linear",dither_type="none", nominal_luminance=peak)
+	o=c
+	a=c
 
-def benchmark(optIN : vs.VideoNode, w=1280, h=720):
+	source_peak=peak 
+	LDR_nits=100     
+	exposure_bias=source_peak/LDR_nits
+
+	if (brightness < 0):
+		adjustIt = False
+	else:
+		adjustIt = True
+	
+	if (brightness > 100):
+		brightness = 100
+
+	if (desat < 0) :
+		desat=0
+	if (desat > 100) :
+	   desat=100
+	desat=desat/100
+
+	tm=((1*(0.15*1+0.10*0.50)+0.20*0.02) / (1*(0.15*1+0.50)+0.20*0.30)) - 0.02/0.30
+	w=((exposure_bias*(0.15*exposure_bias+0.10*0.50)+0.20*0.02)/(exposure_bias*(0.15*exposure_bias+0.50)+0.20*0.30))-0.02/0.30
+	tm_ldr_value=tm * (1 / w)#value of 100 nits after the tone mapping
+	ldr_value_mult=tm_ldr_value/(1/exposure_bias)#0.1 (100nits) * ldr_value_mult=tm_ldr_value
+
+	tm = core.std.Expr(c, expr="x  {exposure_bias} * 0.15 x  {exposure_bias} * * 0.05 + * 0.004 + x  {exposure_bias} * 0.15 x  {exposure_bias} * * 0.50 + * 0.06 + / 0.02 0.30 / -  ".format(exposure_bias=exposure_bias),format=vs.RGBS)
+	w=((exposure_bias*(0.15*exposure_bias+0.10*0.50)+0.20*0.02)/(exposure_bias*(0.15*exposure_bias+0.50)+0.20*0.30))-0.02/0.30
+	tm = core.std.Expr(clips=[tm,c], expr="x  1 {w}  / * ".format(exposure_bias=exposure_bias,w=w),format=vs.RGBS)
+	tm = core.std.Limiter(tm, 0, 1)
+
+	if lin == True :
+		#linearize the tonemapper curve under 100nits
+		tm=core.std.Expr(clips=[tm,o], expr="x {tm_ldr_value} < y {ldr_value_mult} * x ? ".format(tm_ldr_value=tm_ldr_value,ldr_value_mult=ldr_value_mult))
+
+
+	a = core.std.Expr(a, expr="x  {ldr_value_mult} *   ".format(ldr_value_mult=ldr_value_mult),format=vs.RGBS)
+	
+
+	r=core.std.ShufflePlanes(clips=[a], planes=[0], colorfamily=vs.GRAY)
+	g=core.std.ShufflePlanes(clips=[a], planes=[1], colorfamily=vs.GRAY)
+	b=core.std.ShufflePlanes(clips=[a], planes=[2], colorfamily=vs.GRAY)
+	#luminance
+	l=core.std.Expr(clips=[r,g,b], expr="x 0.2627 *  y 0.678 * + z 0.0593 * +    ",format=vs.GRAY)
+
+	#value under 100nits after the tone mapping(tm_ldr_value) becomes 0, even tm_ldr_value becomes 0 and then scale all in the 0-1 range fo the mask 
+	mask=core.std.Expr(clips=[l], expr="x {tm_ldr_value}  - {ldr_value_mult} /".format(ldr_value_mult=ldr_value_mult,tm_ldr_value=tm_ldr_value))
+	mask = core.std.Limiter(mask, 0, 1)
+
+	#reduce the saturation blending with grayscale
+	lrgb=core.std.ShufflePlanes(clips=[l,l,l], planes=[0,0,0], colorfamily=vs.RGB)
+	asat=core.std.Expr(clips=[a,lrgb], expr="y {tm_ldr_value} < x  y {desat} * x 1 {desat} - * +   ?".format(tm_ldr_value=tm_ldr_value,desat=desat))
+	a=core.std.MaskedMerge(a, asat, mask)
+
+	r=core.std.ShufflePlanes(clips=[a], planes=[0], colorfamily=vs.GRAY)
+	g=core.std.ShufflePlanes(clips=[a], planes=[1], colorfamily=vs.GRAY)
+	b=core.std.ShufflePlanes(clips=[a], planes=[2], colorfamily=vs.GRAY)
+
+	rl=core.std.ShufflePlanes(clips=[tm], planes=[0], colorfamily=vs.GRAY)
+	gl=core.std.ShufflePlanes(clips=[tm], planes=[1], colorfamily=vs.GRAY)
+	bl=core.std.ShufflePlanes(clips=[tm], planes=[2], colorfamily=vs.GRAY)
+	l2=core.std.Expr(clips=[rl,gl,bl], expr="x 0.2627 *  y 0.678 * + z 0.0593 * +    ",format=vs.GRAY)
+	nl= l2
+	scale=core.std.Expr(clips=[nl,l], expr="x y / ")
+	r1=core.std.Expr(clips=[r,scale], expr="x  y *")
+	g1=core.std.Expr(clips=[g,scale], expr="x  y *")
+	b1=core.std.Expr(clips=[b,scale], expr="x  y *")
+
+	c=core.std.ShufflePlanes(clips=[r1,g1,b1], planes=[0,0,0], colorfamily=vs.RGB)
+	c = core.std.Limiter(c, 0, 1)
+
+	if show_satmask == True :
+		#show mask
+		c=core.std.ShufflePlanes(clips=[mask,mask,mask], planes=[0,0,0], colorfamily=vs.RGB)
+
+	if show_clipped == True :
+		#show clipped
+		c=a
+	
+	#then
+	c=core.resize.Bicubic(clip=c, format=vs.YUV420P10, filter_param_a=0.0, filter_param_b=1.0, matrix_s="709", primaries_in_s="2020", primaries_s="709", transfer_in_s="linear", transfer_s="709",dither_type="ordered")
+
+	#some adjustment
+	#tewak.Tweak
+	#adjust.Tweak(bright=1.0)
+	if adjustIt:
+		if (cont < 0):
+			cont = 1.05
+		if (cont > 100):
+			cont = 100
+		c=adjust.Tweak(c,bright=brightness,cont=cont)
+	return c 
+
+
+
+def benchmark(optIN, w=1280, h=720):
 	"""
 	Sole PC test purpose lol
 	optIN is Video
 	w: width
 	h: height
 	default is 1280x720
-	Just add yourvar.set_output() after using this script
 	"""
 	src = fvf.Depth(optIN, 16)
 	rpow = edi.nnedi3_rpow2(src,rfactor=2) #why not?
@@ -258,8 +349,14 @@ def benchmark(optIN : vs.VideoNode, w=1280, h=720):
 	i444 = fvf.Downscale444(deband, w=w, h=h, kernely="blackmanminlobe", kerneluv="blackmanminlobe")
 	aa = taa.TAAmbk(i444,aatype='Eedi3',cycle=1)
 	finalmeme = fvf.Depth(aa, 10)
-	return finalmeme
+	return finalmeme.set_output()
+
+#aliases
+tonemap = hdr2sdr
+autistCode = benchmark
+ivtc = animu
 
 """
 (c) 2018 Autist version of N4O
 """
+
