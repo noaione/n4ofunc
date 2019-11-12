677ac91ea7d1b17f0494b2e10fc3dda61f878cab
NoAiOne <aimanmh@my.smccd.edu>
2019-11-12 20:45:18 +0700

    Code cleanup and function removal

--- n4ofunc.py
+++ n4ofunc.py
@@ -7,8 +7,7 @@ import kagefunc as kgf
 import mvsfunc as mvf
 import vapoursynth as vs
 
-from vsutil import get_y, is_image, iterate, split, get_w, frame2clip
-from math import ceil, log
+from vsutil import get_y, is_image, iterate, split, get_w
 
 core = vs.core
 
@@ -43,101 +42,7 @@ def register_f(c: vs.VideoNode, yuv444=False) -> vs.VideoNode.format:
         0 if yuv444 else c.format.subsampling_w
     )
 
-
-def nDeHalo(clp=None, rx=None, ry=None, darkstr=None, brightstr=None, lowsens=None, highsens=None, ss=None, thr=None):
-    """
-    # Dehalo filter based on Dehalo_alpha and custom simple mask
-    ###################################################################
-    # rx, ry [float, 1.0 ... 2.0 ... ~3.0]
-    # As usual, the radii for halo removal.
-    # Note: this function is rather sensitive to the radius settings. Set it as low as possible! If radius is set too high, it will start missing small spots.
-    #
-    # darkkstr, brightstr [float, 0.0 ... 1.0] [<0.0 and >1.0 possible]
-    # The strength factors for processing dark and bright halos. Default 1.0 both for symmetrical processing.
-    # On Comic/Anime, darkstr=0.4~0.8 sometimes might be better ... sometimes. In General, the function seems to preserve dark lines rather good.
-    #
-    # lowsens, highsens [int, 0 ... 50 ... 100]
-    # Sensitivity settings, not that easy to describe them exactly ...
-    # In a sense, they define a window between how weak an achieved effect has to be to get fully accepted, and how strong an achieved effect has to be to get fully discarded.
-    # Defaults are 50 and 50 ... try and see for yourself.
-    #
-    # ss [float, 1.0 ... 1.5 ...]
-    # Supersampling factor, to avoid creation of aliasing.
-    #
-    # thr [int, 1 ... 65535]
-    # Maximum threshold factor
-    # Recommended: 15000+
-    # Max: Clip Sample Value (65535)
-    """
-    import math
-
-    def m4(x):
-        return 16 if x < 16 else math.floor(x / 4 + 0.5) * 4
-    def scale(value, peak):
-        return value * peak // 255
-
-    # Defaults
-    rx = 2. if rx is None else rx
-    ry = 2. if ry is None else ry
-    darkstr = 1. if darkstr is None else darkstr
-    brightstr = 1. if brightstr is None else brightstr
-    lowsens = 50 if lowsens is None else lowsens
-    highsens = 50 if highsens is None else highsens
-    ss = 1.5 if ss is None else ss
-    thr = 65535 if thr is None else thr
-
-    # Error Check
-    if not isinstance(clp, vs.VideoNode):
-        raise ValueError("nDeHalo: This is not a clip")
-    if thr > 65535:
-        raise ValueError("nDeHalo: thr cannot exceed 65535: {x}".format(x=thr))
-
-    # Mask creation
-    sx = clp.std.Convolution([-1, -2, -1, 0, 0, 0, 1, 2, 1], saturate=False)
-    sy = clp.std.Convolution([-1, 0, 1, -2, 0, 2, -1, 0, 1], saturate=False)
-    inner = core.std.Expr([sx, sy], 'x y max').std.ShufflePlanes(0, vs.GRAY)
-    outer = inner.std.Maximum(threshold=thr)
-    mask = core.std.Expr([outer, inner], 'x y -')
-
-    peak = (1 << clp.format.bits_per_sample) - 1
-
-    # Initial Check
-    if clp.format.color_family != vs.GRAY:
-        clp_orig = clp
-        clp = core.std.ShufflePlanes(clp, 0, vs.GRAY)
-    else:
-        clp_orig = None
-
-    ox = clp.width
-    oy = clp.height
-
-    # DeHalo
-    halos = core.resize.Bicubic(clp, m4(ox / rx), m4(oy / ry)).resize.Bicubic(ox, oy, filter_param_a=1, filter_param_b=0)
-    are = core.std.Expr([core.std.Maximum(clp), core.std.Minimum(clp)], ['x y -'])
-    ugly = core.std.Expr([core.std.Maximum(halos), core.std.Minimum(halos)], ['x y -'])
-    expr = 'y x - y / {peak} * {LOS} - y {i} + {j} / {HIS} + *'.format(peak=peak, LOS=scale(lowsens, peak), i=scale(256, peak), j=scale(512, peak), HIS=highsens / 100)
-    so = core.std.Expr([ugly, are], [expr])
-    lets = core.std.MaskedMerge(halos, clp, so)
-    if ss <= 1:
-        remove = core.rgvs.Repair(clp, lets, 1)
-    else:
-        remove = core.std.Expr([core.std.Expr([core.resize.Spline36(clp, m4(ox * ss), m4(oy * ss)),
-                                               core.std.Maximum(lets).resize.Bicubic(m4(ox * ss), m4(oy * ss))],
-                                              ['x y min']),
-                                core.std.Minimum(lets).resize.Bicubic(m4(ox * ss), m4(oy * ss))],
-                               ['x y max']).resize.Spline36(ox, oy)
-    them = core.std.Expr([clp, remove], ['x y < x x y - {DRK} * - x x y - {BRT} * - ?'.format(DRK=darkstr, BRT=brightstr)])
-
-    # Merge
-    if clp_orig is not None:
-        final = core.std.ShufflePlanes([them, clp_orig], planes=[0, 1, 2], colorfamily=clp_orig.format.color_family)
-        fc = clp_orig
-    else:
-        final = them
-        fc = clp
-    return core.std.MaskedMerge(fc, final, mask)
-
-
+# TODO: Merge with save_difference
 def check_diff(src1: vs.VideoNode, src2: vs.VideoNode, threshold: float = 0.1):
     src1_cf = src1.format.color_family
     src2_cf = src2.format.color_family
@@ -246,13 +151,6 @@ def save_difference(src1: vs.VideoNode, src2: vs.VideoNode, threshold: float = 0
             out.get_frame(0)
     except KeyboardInterrupt:
         print('[!!] CTRL+C Pressed, stopping...')
-
-
-def adaptive_degrain(src, *args, **kwargs):
-    """
-    Placeholder
-    """
-    raise Exception('This function has been deleted, and moved to adaptive_degrain2 with better support for other degrainer, check the docstring for more info')
 
 
 def adaptive_degrain2(src, luma_scaling=None, kernel='smdegrain', area='light', iter_edge=0, show_mask=False, **degrain_args):
@@ -501,12 +399,6 @@ def source(src, lsmas=False, depth=False, trims=None, dither_yuv=True) -> vs.Vid
     def parse_trim_data(trim_data, video):
         a, b = trim_data
 
-        # Every retarded streaming sites ever
-        if a == 'funi':
-            a = 240
-        if b == 'sentai':
-            b = video.num_frames - 1458 # May be changed idk
-
         if b == 0:
             b = video.num_frames
         else:
@@ -534,8 +426,7 @@ def source(src, lsmas=False, depth=False, trims=None, dither_yuv=True) -> vs.Vid
     if not isinstance(src, str):
         return ValueError('src: must be string input')
 
-    OpenSource = Source(src, lsmas)
-    src = OpenSource(src)
+    src = Source(src, lsmas)(src)
 
     if dither_yuv and src.format.color_family != vs.YUV:
         src = core.resize.Point(src, format=vs.YUV420P8, matrix_s='709')
@@ -625,6 +516,7 @@ def adaptive_scaling(clip: vs.VideoNode, target_w=None, target_h=None, descale_r
         return down, diff
 
     ref = clip
+    ref_d = ref.format.bits_per_sample
     clip32 = fvf.Depth(clip, 32)
     y = get_y(clip32)
     global_clip_resizer = VideoResizer(b, c, taps, kernel)
@@ -682,12 +574,12 @@ def adaptive_scaling(clip: vs.VideoNode, target_w=None, target_h=None, descale_r
 
     merged = core.std.MaskedMerge(y, y_deb, mask)
     merged = core.std.ShufflePlanes([merged, clip32], [0, 1, 2], vs.YUV)
-    return fvf.Depth(merged, 16)
+    return fvf.Depth(merged, ref_d)
 
 
 def SimpleFrameReplace(src: vs.VideoNode, src_frame: int, target_frame: str) -> vs.VideoNode:
     """
-    A simple frame replacing, useful for replacing black frame with other frame from the same video
+    A simple frame replacing, useful for replacing black frame with other frame from the same video since I'm lazy
     :param src: vapoursynth.VideoNode: Video Source
     :param src_frame: int: Video Frame number as Source for replacing
     :param target_frame: str: Video Target frame to be replaced from src_frame
@@ -696,7 +588,7 @@ def SimpleFrameReplace(src: vs.VideoNode, src_frame: int, target_frame: str) -> 
     src_fpsnum = src.fps.numerator
     src_fpsden = src.fps.denominator
 
-    src_frame = frame2clip(src.get_frame(src_frame)).std.AssumeFPS(fpsnum=src_fpsnum, fpsden=src_fpsden)
+    src_frame = src[src_frame]
 
     frame_range = target_frame.split('-')
 
@@ -838,7 +730,7 @@ def better_planes(clips: vs.VideoNode, props="avg", show_info=False):
 
     return core.std.ShufflePlanes([y_val, u_val, v_val], [0], vs.YUV)
 
-# TODO: Maybe add chroma support or something
+# TODO: Maybe add chroma support or something and try to merge with better_planes
 def better_frame(clips: vs.VideoNode, props="avg", show_info=False):
     """
     A naive function for picking the best frames from a list of video (Basically better_planes without checking chroma plane)
@@ -942,187 +834,3 @@ adaptive_descale = partial(adaptive_scaling, rescale=False)
 sfr = SimpleFrameReplace
 bplanes = better_planes
 bframe = better_frame
-
-
-######### Below here is mpeg2stinx script #########
-
-# Note: Not working currently, help me
-
-def spline64bob(src, process_chroma=True):
-    def bob(src):
-        src = src.fmtc.resample(w=src.width, h=src.height, kernel="spline64", css="420").fmtc.bitdepth(bits=8)
-        src = src.std.SeparateFields(True)[::2]
-        e = core.std.SelectEvery(src, cycle=2, offsets=0).fmtc.resample(w=src.width, h=2 * src.height, kernel="spline64", sx=0, sy=0.25, sw=src.width, sh=src.height).fmtc.bitdepth(bits=8)
-        o = core.std.SelectEvery(src, cycle=2, offsets=1).fmtc.resample(w=src.width, h=2 * src.height, kernel="spline64", sx=0, sy=-0.25, sw=src.width, sh=src.height).fmtc.bitdepth(bits=8)
-        return core.std.Interleave(clips=[e, o])
-    if src.format == 'YUV420P8':
-        return bob(src)
-    y = core.std.ShufflePlanes(clips=src, planes=0, colorfamily=vs.GRAY)
-    u = core.std.ShufflePlanes(clips=src, planes=1, colorfamily=vs.GRAY)
-    v = core.std.ShufflePlanes(clips=src, planes=2, colorfamily=vs.GRAY)
-    if process_chroma:
-        return core.std.ShufflePlanes([y, bob(u), bob(v)], planes=[0, 0, 0], colorfamily=vs.YUV)
-    return core.std.ShufflePlanes([y, core.std.SelectEvery(u, cycle=1, offsets=[0, 0]), core.std.SelectEvery(v, cycle=1, offsets=[0, 0])], planes=[0, 0, 0], colorfamily=vs.YUV)
-
-
-def pointbob(src):
-    src = src.std.SeparateFields(True)[::2]
-    return core.resize.Point(src, src.width, 2*src.height)
-
-
-def median3(a, b, c, grey=True):
-    return core.std.Interleave([a, b, c]).rgvs.Clense(planes=[0, 1, 2] if grey is True else 0).std.SelectEvery(cycle=3, offsets=1)
-
-
-def crossfieldrepair(clip, sw=2, sh=2, bobbed_clip=None, chroma=True):
-    if (sw < 0 and sh < 0) or sw < 0 or sh < 0:
-        raise ValueError("crossfieldrepair: sw/sh cannot be a negative integers")
-    if not isinstance(clip, vs.VideoNode):
-        raise vs.Error("crossfieldrepair:\"clip\" not a clip")
-    if bobbed_clip is None:
-        bobbed_clip = spline64bob(clip, process_chroma=chroma)
-    bob_ex = haf.mt_expand_multi(bobbed_clip, planes=1, sw=sw, sh=sh)
-    bob_in = haf.mt_inpand_multi(bobbed_clip, planes=1, sw=sw, sh=sh)
-    if sw == 1 and sh == 1:
-        e = core.std.SelectEvery(bobbed_clip, cycle=2, offsets=0)
-        o = core.std.SelectEvery(bobbed_clip, cycle=2, offsets=1)
-        re = core.rgvs.Repair(bobbed_clip, e, mode=[1])
-        ro = core.rgvs.Repair(bobbed_clip, o, mode=[1])
-    else:
-        ex = core.std.SelectEvery(bob_ex, cycle=2, offsets=0)
-        ox = core.std.SelectEvery(bob_ex, cycle=2, offsets=1)
-        ei = core.std.SelectEvery(bob_in, cycle=2, offsets=0)
-        oi = core.std.SelectEvery(bob_in, cycle=2, offsets=1)
-        re = median3(bobbed_clip, ex, ox, False)
-        ro = median3(bobbed_clip, ei, oi, False)
-    res = core.std.Interleave(clips=[re, ro])
-    res = res.std.SeparateFields(True)[::2]
-    res = core.std.SelectEvery(res, cycle=4, offsets=[2, 1])
-    res = res.std.DoubleWeave()[::2]
-    return res
-
-
-def maxyuv(c):
-    y = c.resize.Bicubic(width=c.width, height=c.height, format=vs.YUV420P8)
-    u = core.resize.Bicubic(core.std.ShufflePlanes(clips=c, planes=1, colorfamily=vs.GRAY), c.width, c.height, vs.YUV420P8)
-    v = core.resize.Bicubic(core.std.ShufflePlanes(clips=c, planes=2, colorfamily=vs.GRAY), c.width, c.height, vs.YUV420P8)
-    w = c.width
-    h = c.height
-
-    yc = core.resize.Bilinear(y, u.width, u.height)
-    ls = core.std.Expr([y, core.resize.Bilinear(u, w, h)], expr=['x y max']).std.Expr([y, core.resize.Bilinear(v, w, h)], expr=['x y max'])
-    cs = core.std.Expr([yc, u], expr=['x y max']).std.Expr([yc, v], expr=['x y max'])
-    return core.std.ShufflePlanes([cs, cs, ls], planes=[0, 0, 0], colorfamily=vs.YUV)
-
-
-def mpeg2stinx(src, mode=1, sw=1, sh=1, contra=True, blurv=0.0, sstr=2.0, scl=0.25, dither=False, order=0, diffscl=None):
-    """
-    #####################################################
-    ###                                               ###
-    ###        mpeg2stinx port for VapourSynth        ###
-    ###                                               ###
-    ###   		  ported by NoAiOne or N4O            ###
-    ###   		  originally by torchlight            ###
-    ###                                               ###
-    ###                                               ###
-    #####################################################
-    ### This filter is designed to eliminate certain combing-like compression artifacts
-    ### that show up all too often in hard-telecined MPEG-2 encodes,
-    ### and works to a smaller extent on bitrate-starved hard-telecined AVC encodes as well.
-    ###
-    ###
-    ### +---------+
-    ### |  USAGE  |
-    ### +---------+
-    ###
-    ### mpeg2stinx(clip, mode, sw, sh, contra, blurv, sstr, scl, dither, order, diffscl)
-    ###
-    ### clip: video Source
-    ### mode: Resizer used for interpolating fields to full size. (0 to 2)
-    ### sw/sh: Parameters for the size of the rectangle on which to perform min/max clipping
-    ### contra: Whether to use contrasharpening.
-    ### blurv: How much vertical blur to apply.
-    ### sstr: Contrasharpening strength.
-    ### scl: Contrasharpening scale.
-    ### dither: Whether to dither when averaging two clips.
-    ### order: Field order to use for yadifmod.
-    ### diffscl: If specified, temporal limiting is used, where the changes by crossfieldrepair
-    ### 		 are limited to diffscl times the difference between the current frame and its neighbours.
-    """
-
-    if not isinstance(src, vs.VideoNode):
-        raise ValueError('mpeg2stinx: src is not a video')
-    if sw < 0 or sh < 0:
-        raise ValueError('mpeg2stinx: sw/sh cannot be a negative integers')
-    if mode < 0 or mode > 3:
-        raise ValueError('mpeg2stinx: mode must be 0, 1 or 2')
-    if order < 0 or order > 2:
-        raise ValueError('mpeg2stinx: order must be 0, 1 or 2')
-    if diffscl is not None and diffscl >= 0:
-        raise ValueError('mpeg2stinx: diffscl must be a negative integers')
-    if contra:
-        blurv = 1.0
-    else:
-        blurv = 0.0
-
-    def deint(src, mode, order):
-        if mode == 0:
-            bobbed = pointbob(src)
-        elif mode == 1:
-            bobbed = spline64bob(src)
-        elif mode == 2:
-            bobbed = core.nnedi3.nnedi3(src, field=3)
-
-        if order == 0:
-            return bobbed
-        elif order == 1:
-            return core.std.SelectEvery(core.yadifmod.Yadifmod(src, order=0, mode=3, edeint=core.std.SelectEvery(bobbed, 2, [1, 0])).selectevery(2, 1, 0), 2, [1, 0])
-        elif order == 2:
-            return core.yadifmod.Yadifmod(src, order=1, mode=3, edeint=bobbed)
-        raise ValueError('mpeg2stinx.deint: order can only be 0, 1, and 2')
-
-    def templimit(c, flt, ref, diffscl):
-        adj = ref.std.SelectEvery(2, [0, 1])
-        diff = core.std.Expr(core.std.SelectEvery(c, 3, [0, 1]), adj, ["x y - abs"])
-        diff = core.std.SeparateFields(True)[::2]
-        diff = maxyuv(diff)
-        diff2 = core.std.Expr(core.std.SelectEvery(diff, 4, [0, 1]), core.std.SelectEvery(diff, 4, [2, 3]), expr=["x y min"])
-        diff2 = haf.mt_expand_multi(diff2, sw=2, sh=1, planes=0)
-        diff2 = diff2.std.DoubleWeave()[::2]
-        a = core.misc.AverageFrames(clips=[c, diff2], weights=[1, -diffscl])
-        b = core.misc.AverageFrames(clips=[c, diff2], weights=[1, diffscl])
-        return median3(a, b, flt)
-
-    a = crossfieldrepair(src, sw=sw, sh=sh, bobbed_clip=deint(src, mode, order))
-    if diffscl is not None:
-        a = templimit(src, a, src, diffscl)
-    b = crossfieldrepair(a, sw=sw, sh=sh, bobbed_clip=deint(a, mode, order))
-    if diffscl is not None:
-        b = templimit(a, b, src, diffscl)
-
-    if dither:
-        dit = core.misc.AverageFrames(clips=[a, b], weights=[0.5, 0.5])
-        dit = mvf.Depth(dit, dither=4)
-    else:
-        dit = core.misc.AverageFrames(clips=[a, b], weights=[1, 1])
-
-    if blurv > 0.0:
-        nuked = core.std.BoxBlur(dit, hradius=1, vradius=blurv)
-    else:
-        nuked = dit
-
-    nukedd = core.std.MakeDiff(src, nuked, [0, 1, 2])
-
-    sharp = core.std.Expr(nuked, core.std.BoxBlur(nuked, hradius=0, vradius=1).std.BoxBlur(nuked, hradius=0, vradius=0), expr=["x x y - {} * +".format(sstr)])
-    sharpd = core.std.Expr(nuked, core.std.BoxBlur(nuked, hradius=0, vradius=1).std.BoxBlur(nuked, hradius=0, vradius=0), expr=["x y - {} * 128 +".format(sstr)])
-    limd = core.std.Expr(sharpd, nukedd, expr=[f"x 128 - y 128 - * 0 < {scl} 1 ? x 128 - abs y 128 - abs < x y ? 128 - * 128 +"])
-
-    if scl == 0:
-        last = median3(nuked, sharp, src)
-    else:
-        last = core.std.MergeDiff(nuked, limd, [0, 1, 2])
-
-    if contra:
-        return last
-    return nuked
-
