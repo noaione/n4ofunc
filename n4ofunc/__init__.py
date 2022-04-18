8e8be84b702c2f0562e2cbf8b84c54baeb357b3c
noaione <noaione0809@gmail.com>
2022-04-18 15:58:57 +0700

    Bump version to 0.1.1

--- n4ofunc/__init__.py
+++ n4ofunc/__init__.py
@@ -17,7 +17,7 @@ from .utils import *
 from .video import *
 
 __title__ = "n4ofunc"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "noaione"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020-present noaione"
